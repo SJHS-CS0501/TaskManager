1f61d013c56fefa8bbd176891758c9bae44c17b2
rylucs17 <ryluchs17@saintjosephhs.com>
2016-01-07 09:33:18 -0500

    did some planning and created the data fields for the Task class

---
+++ TaskManager.java
@@ -0,0 +1,48 @@
+/**
+ * A program that manages tasks
+ * @author Ryan Luchs
+ * 
+ */
+
+/*
+ * Task Manager Program
+ * 
+ * Do these things:
+ * - Keep a list of tasks
+ * - Allow user to add and remove tasks
+ * - Allow user to reprioritize tasks
+ * - Print out lists of tasks
+ * 
+ * List of tasks:
+ * - Read and Store on disk
+ * - Sort
+ * - Search
+ * 
+ * Reports:
+ * - List high priority
+ * - List due today/soon
+ * - List all (by date OR by priority)
+ * 
+ */
+
+/**
+ * TaskManager main class
+ * @author Ryan Luchs
+ *
+ */
+public class TaskManager {
+
+	/**
+	 * TaskManager main method
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("welcome to the Task Manager!");
+		
+		
+		
+		System.exit(0);
+	}
+
+}
