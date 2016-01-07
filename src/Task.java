56ab69a7fcb462f283175bb8a2568f2307e31403
jprotivnak <jprotivnak@me.com>
2016-01-07 18:46:43 -0500

    Changes
    Did all accessors and mutators and empty constructor.

--- src/Task.java
+++ src/Task.java
@@ -11,6 +11,57 @@ import java.util.*;
  *
  */
 public class Task {
+	
+	public static void Task() {
+	}
+	
+	public static void setPriority(short userPriority) {
+		priority = userPriority;
+	}
+	
+	public static short getPriority() {
+		return priority;
+	}
+	
+	public static void setDueDate(Date userDate) {
+		dueDate = userDate;
+	}
+	
+	public static Date getDate() {
+		return dueDate;
+	}
+	
+	public static void setCategory(short userCategory) {
+		category = userCategory;
+	}
+	
+	public static short getCategory() {
+		return category;
+	}
+	
+	public static void setDescription(String userDescription) {
+		description = userDescription;
+	}
+	
+	public static String getDescription() {
+		return description;
+	}
+	
+	public static void setLocation(String userLocation) {
+		location = userLocation;
+	}
+	
+	public static String getLocation() {
+		return location;
+	}
+	
+	public static void setCompleted(boolean userCompleted) {
+		completed = userCompleted;
+	}
+	
+	public static boolean getCompleted() {
+		return completed;
+	}
 	/*
 	 * Data fields:
 	 * - Priority
@@ -30,6 +81,23 @@ public class Task {
 	/*
 	 * Priorities
 	 */
+	 
+	public static short getHigh() {
+		return PRIO_HIGH;
+	}
+	
+	public static short getMedium() {
+		return PRIO_MED;
+	}
+	
+	public static short getLow() {
+		return PRIO_LOW;
+	}
+	
+	public static short getUndefinedPriority() {
+		return PRIO_UNDEF;
+	}
+	
 	public static final short PRIO_HIGH = 1;
 	public static final short PRIO_MED = 2;
 	public static final short PRIO_LOW = 3;
@@ -38,6 +106,30 @@ public class Task {
 	/*
 	 * Categories
 	 */
+	 
+	public static short getUndefinedCategory() {
+		return CAT_UNDEF;
+	}
+	
+	public static short getOther() {
+		return CAT_OTHER;
+	}
+	
+	public static short getSchool() {
+		return CAT_SCHOOL;
+	}
+	
+	public static short getPersonal() {
+		return CAT_PERSONAL;
+	}
+	
+	public static short getChore() {
+		return CAT_CHORE;
+	}
+	
+	public static short getWork() {
+		return CAT_WORK;
+	}
 	public static final short CAT_UNDEF = 0;
 	public static final short CAT_OTHER = 1;
 	public static final short CAT_SCHOOL = 2;
@@ -53,6 +145,4 @@ public class Task {
 	 * - toString
 	 * - equals
 	 */
-	
-	
 }
