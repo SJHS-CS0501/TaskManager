22b6dad9b8a013bb08be5f358564175019b9ab20
isschroeder <kittypuppybird@gmail.com>
2016-01-07 09:32:07 -0500

    Class Stuff

---
+++ Task.java
@@ -0,0 +1,54 @@
+import java.util.Date;
+
+/**
+ * 
+ * Task object containing information on a single task.
+ * @author Isabelle Schroeder
+ *
+ */
+public class Task {
+	/*
+	 * Data fields:
+	 *  -Priority
+	 *  -Due date
+	 *  -Category
+	 *  -Description
+	 *  -Location
+	 *  -Completed
+	 */
+	private short priority;
+	private Date dueDate;
+	private short category;
+	private String description;
+	private String location;
+	private boolean completed;
+	
+	/*
+	 * priorities
+	 */
+	public static final short PRIO_HIGH = 1;
+	public static final short PRIO_MED = 2;
+	public static final short PRIO_LOW = 3;
+	public static final short PRIO_UNDEF = 0;
+	
+	
+	/*
+	 * categories
+	 */
+	public static final short CAT_UNDEF = 0;
+	public static final short CAT_OTHER = 1;
+	public static final short CAT_SCHOOL = 2;
+	public static final short CAT_PERSONAL = 3;
+	public static final short CAT_CHORE = 4;
+	public static final short CAT_WORK = 5;
+	
+	
+	
+	/* 
+	 * Methods:
+	 * - Empty Constructor
+	 * - Accessors/Mutators for all fields
+	 * - toString
+	 * - string.equals to compare tasks 
+	 */
+}
---
+++ TaskManager.java
@@ -0,0 +1,36 @@
+/*
+ * Task Manager Program 
+ * 
+ * DO These Things:
+ * -Keep a list of tasks
+ * -Allow user to add and remove tasks
+ * -Allow user to reprioratize tasks
+ * -Print out lists of tasks
+ * 
+ * List of tasks
+ * -Read and store on disk
+ * -Sort
+ * -Search
+ * 
+ * Reports:
+ * -List high priority
+ * -List due today/soon
+ * - List all (by date OR by priority)
+ */
+
+/**
+ * 
+ * @author Isabelle Schroeder
+ *
+ */
+public class TaskManager {
+
+	public static void main(String[] args) {
+
+		System.out.println( "Welcome to the Task Manager!" );
+		
+		
+		System.exit(0);
+	}
+
+}
