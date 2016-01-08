cdb4cb71b9076cdc739faa7c35c21b3bf959fcff
rylucs17 <ryluchs17@saintjosephhs.com>
2016-01-08 10:17:43 -0500

    wrote accessors and mutators in the Task class

--- Task.java
+++ Task.java
@@ -49,5 +49,145 @@ public class Task {
 	 * - equals
 	 * 
 	 */
-
+	
+	Task() {
+	}
+	
+	/**
+	 * Sets task priority
+	 * @param p The priority
+	 */
+	public void setPriority(short p) {
+		switch (p) {
+			case 0:
+				priority = PRIO_UNDEF;
+				break;
+			case 1:
+				priority = PRIO_HIGH;
+				break;
+			case 2:
+				priority = PRIO_MED;
+				break;
+			case 3:
+				priority = PRIO_LOW;
+				break;
+			default:
+				priority = PRIO_UNDEF;
+				System.out.println("Priority of (" + p + ") is undefinded.");
+				break;
+		}
+	}
+	
+	/**
+	 * Gets task priority
+	 * @return Task priority
+	 */
+	public short getPriority() {
+		return priority;
+	}
+	
+	/**
+	 * Sets due date in year/month/day format
+	 * @param y Year
+	 * @param m Month
+	 * @param d Date
+	 */
+	public void setDueDate(int y, int m, int d) {
+		dueDate = new Date(y, m, d);
+	}
+	
+	/**
+	 * Gets the task's due date
+	 * @return The task's due date
+	 */
+	public Date getDueDate() {
+		return dueDate;
+	}
+	
+	/**
+	 * Sets task category
+	 * @param c The category
+	 */
+	public void setCategory(short c) {
+		switch (c) {
+			case 0:
+				category = CAT_UNDEF;
+				break;
+			case 1:
+				 category = CAT_OTHER;
+				 break;
+			case 2:
+				 category = CAT_SCHOOL;
+				 break;
+			case 3:
+				 category = CAT_PERSONAL;
+				 break;
+			case 4:
+				 category = CAT_CHORE;
+				 break;
+			case 5:
+				category = CAT_WORK;
+				break;
+			default:
+				category = CAT_UNDEF;
+				System.out.println("Category (" + c + ") is undefined.");
+				break;
+		}
+	}
+	
+	/**
+	 * Gets the task's category
+	 * @return The task's category		
+	 */
+	public short getCategory() {
+		return category;
+	}
+	
+	/**
+	 * Sets task description
+	 * @param d The description
+	 */
+	public void setDescription(String d) {
+		description = new String(d);
+	}
+	
+	/**
+	 * Gets task description
+	 * @return The task description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * Sets task location
+	 * @param l The location
+	 */
+	public void setLocation(String l) {
+		location = new String(l);
+	}
+	
+	/**
+	 * Gets task location
+	 * @param The task location
+	 */
+	public String getLocation() {
+		return location;
+	}
+	
+	/**
+	 * Sets whether task is completed or not
+	 * @param c Completion, true/false
+	 */
+	public void setCompleted(boolean c) {
+		completed = c;
+	}
+	
+	/**
+	 * Gets the completion state of the task
+	 * @return Whether the task is completed true/false
+	 */
+	public Boolean getCompleted() {
+		return completed;
+	}
 }
