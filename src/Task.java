c4cfdbd2dc4f735efaa860855bba0a9c4d3aeb82
juliannanichols <junichols17@saintjosephhs.com>
2016-01-10 13:13:19 -0500

    methods
    get/set

--- src/Task.java
+++ src/Task.java
@@ -15,6 +15,54 @@ public class Task {
 	 * - completion
 	 */
 	
+	public void setPriority( short userPriority ) {
+		userPriority = priority;
+	}
+	
+	public short getPriority() {
+		return priority;
+	}
+	
+	public void setDueDate( Date userDueDate ) {
+		userDueDate = dueDate;
+	}
+	
+	public Date getDueDate() {
+		return dueDate;
+	}
+	
+	public void setCategory( short userCategory ) {
+		userCategory = category;
+	}
+	
+	public short getCategory() {
+		return category;
+	}
+	
+	public void setDescription( String userDescription ) {
+		userDescription = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setLocation( String userLocation ) {
+		userLocation = location;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public void setCompleted( boolean userCompleted ) {
+		userCompleted = completed;
+	}
+	
+	public boolean getCompleted() {
+		return completed;
+	}
+	
 	private short priority;
 	private Date dueDate;
 	private short category;
@@ -25,6 +73,23 @@ public class Task {
 	/*
 	 * priorities
 	 */
+	
+	private final short getPrio_High() {
+		return ;
+	}
+	
+	private final short getPrio_Med() {
+		return ;
+	}
+	
+	private final short getPrio_Low() {
+		return ;
+	}
+	
+	private final short getPrio_Undef() {
+		return ;
+	}
+	
 	public static final short PRIO_HIGH = 1;
 	public static final short PRIO_MED = 2;
 	public static final short PRIO_LOW = 3;
@@ -33,6 +98,31 @@ public class Task {
 	/*
 	 * categories
 	 */
+	
+	private final short getCat_Undef() {
+		return ;
+	}
+	
+	private final short getCat_Other() {
+		return ;
+	}
+	
+	private final short getCat_School() {
+		return ;
+	}
+	
+	private final short getCat_Personal() {
+		return ;
+	}
+	
+	private final short getCat_Chore() {
+		return ;
+	}
+	
+	private final short getCat_Work() {
+		return ;
+	}
+	
 	public static final short CAT_UNDEF = 0;
 	public static final short CAT_OTHER = 1;
 	public static final short CAT_SCHOOL = 2;
