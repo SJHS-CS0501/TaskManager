60c84f2320a2cfe16138f3ecc5ed90468060dd98
jprotivnak <jprotivnak@me.com>
2016-01-11 08:44:48 -0500

    Stuff

--- src/Task.java
+++ src/Task.java
@@ -21,22 +21,77 @@ public class Task {
 	 * - Location
 	 * - Completed
 	 */
-	private short priority;
+	private short priority;//
 	private Date dueDate;
-	private short category;
+	private short category;//
 	private String description;
 	private String location;
 	private boolean completed;
+
+	/*
+	 * Priorities
+	 */
+	 
+	public static final short PRIO_HIGH = 1;
+	public static final short PRIO_MED = 2;
+	public static final short PRIO_LOW = 3;
+	public static final short PRIO_UNDEF = 0;
+	
+	/*
+	 * Categories
+	 */
+	
+	public static final short CAT_UNDEF = 0;
+	public static final short CAT_OTHER = 1;
+	public static final short CAT_SCHOOL = 2;
+	public static final short CAT_PERSONAL = 3;
+	public static final short CAT_CHORE = 4;
+	public static final short CAT_WORK = 5;
+	
+	
+	/*
+	 * Methods:
+	 * - Empty constructor
+	 * - Accessors/Mutators for all fields
+	 * - toString
+	 * - equals
+	 */
 	
 	public void Task() {
 	}
 	
-	public void setPriority(short userPriority) {
+	/**
+	 * Set the priority of the task.
+	 * @param userPriority short priority value (found in Task)
+	 */
+	
+	public void setPriority(short userPriority) throws IllegalArgumentException{
+		if (userPriority < 0 || userPriority > 3) {
+			throw new IllegalArgumentException( "Invalid priority" );
+		}
 		priority = userPriority;
 	}
 	
 	public short getPriority() {
 		return priority;
+	}
+	
+	public String getPriorityName() {
+		String pName;
+		switch( priority ) {
+			case 0:
+				pName = "Undefined";
+				break;
+			case 1:
+				pName = "High";
+				break;
+			case 2:
+				pName = "Medium";
+				break;
+			case 3:
+				pName = "Low";
+		}
+		return pName;
 	}
 	
 	public void setDueDate(Date userDate) {
@@ -78,12 +133,9 @@ public class Task {
 	public boolean getCompleted() {
 		return completed;
 	}
-
 	
-	/*
-	 * Priorities
-	 */
-	 
+	
+	
 	public static short getHigh() {
 		return PRIO_HIGH;
 	}
@@ -100,15 +152,9 @@ public class Task {
 		return PRIO_UNDEF;
 	}
 	
-	public static final short PRIO_HIGH = 1;
-	public static final short PRIO_MED = 2;
-	public static final short PRIO_LOW = 3;
-	public static final short PRIO_UNDEF = 0;
 	
-	/*
-	 * Categories
-	 */
-	 
+	
+	
 	public static short getUndefinedCategory() {
 		return CAT_UNDEF;
 	}
@@ -132,19 +178,14 @@ public class Task {
 	public static short getWork() {
 		return CAT_WORK;
 	}
-	public static final short CAT_UNDEF = 0;
-	public static final short CAT_OTHER = 1;
-	public static final short CAT_SCHOOL = 2;
-	public static final short CAT_PERSONAL = 3;
-	public static final short CAT_CHORE = 4;
-	public static final short CAT_WORK = 5;
 	
 	
-	/*
-	 * Methods:
-	 * - Empty constructor
-	 * - Accessors/Mutators for all fields
-	 * - toString
-	 * - equals
-	 */
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -31,7 +31,33 @@ public class TaskManager {
 	
 	public static void main( String [] args ) {
 		// main stuff goes here, yo
-		System.out.println( "Welcom to the Task Manager!" );
+		System.out.println( "Welcome to the Task Manager!" );
+		
+		/*
+		 * ERASE FROM HERE TO THERE WHEN DONE TESTING TASK
+		 */
+		Task foo = new Task();
+		foo.setDescription( "Test Task" );
+		try {
+			// What we want to try that might throw an exception
+			foo.setPriority( (short)12 );
+		} catch( Exception e ) {
+			// What to do if we catch the exception
+			System.out.println( "Error setting priority: " + e.getMessage() );
+			//System.exit(-1);
+		}
+		
+		// time passes...
+		
+		System.out.println( "Task info: " );
+		System.out.println( "\tTask: " + foo.getDescription() );
+		System.out.println( "\t\tPriority: " + foo.getPriorityName() );
+		
+		/*
+		 * THERE
+		 */
+		
+		
 		
 		
 		
