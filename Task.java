13eeccfe5a1abfd0cc599dff13e9552889bf9563
rylucs17 <ryluchs17@saintjosephhs.com>
2016-01-14 09:58:54 -0500

    wrote much of TaskList, made minnor changes to Task, wrote write method in Task

--- Task.java
+++ Task.java
@@ -1,8 +1,5 @@
-/**
- * 
- */
-
 import java.util.Date;
+import java.io.PrintWriter;
 
 /**
  * Task object containing information on a single task.
@@ -50,7 +47,7 @@ public class Task {
 	 * 
 	 */
 	
-	Task() {
+	public Task() {
 	}
 	
 	/**
@@ -73,10 +70,36 @@ public class Task {
 	
 	/**
 	 * Gets task priority
-	 * @return Task priority
+	 * @return Task priority value
 	 */
 	public short getPriority() {
 		return priority;
+	}
+	
+	/**
+	 * Returns a String of the name of the task priority
+	 * @return The name of the priority (String)
+	 */
+	public String getPriorityName() {
+		String pName;
+		switch (priority) {
+			case 0:
+				pName = "Undefined";
+				break;
+			case 1:
+				pName = "High";
+				break;
+			case 2:
+				pName = "Medium";
+				break;
+			case 3:
+				pName = "Low";
+				break;
+			default:
+				pName = "ILLEGAL VALUE";
+				break;
+		}
+		return pName;
 	}
 	
 	/**
@@ -91,7 +114,7 @@ public class Task {
 	
 	/**
 	 * Gets the task's due date
-	 * @return The task's due date
+	 * @return Date task's due date
 	 */
 	public Date getDueDate() {
 		return dueDate;
@@ -118,15 +141,47 @@ public class Task {
 	
 	/**
 	 * Gets the task's category
-	 * @return The task's category		
+	 * @return The task's category value		
 	 */
 	public short getCategory() {
 		return category;
 	}
 	
 	/**
+	 * Returns a String of the name of the task category
+	 * @return The name of the category (String)
+	 */
+	public String getCategoryName() {
+		String cName;
+		switch (category) {
+		case 0:
+			cName = "Undefined";
+			break;
+		case 1:
+			cName = "Other";
+			break;
+		case 2:
+			cName = "School";
+			break;
+		case 3:
+			cName = "Personal";
+			break;
+		case 4:
+			cName = "Chore";
+			break;
+		case 5:
+			cName = "Work";
+			break;
+		default:
+			cName = "ILLEGAL VALUE";
+			break;
+		}
+		return cName;
+	}
+	
+	/**
 	 * Sets task description
-	 * @param d The description
+	 * @param d String description
 	 */
 	public void setDescription(String d) {
 		description = new String(d);
@@ -149,8 +204,8 @@ public class Task {
 	}
 	
 	/**
-	 * Gets task location
-	 * @param The task location
+	 * Gets task location string
+	 * @param The String task location
 	 */
 	public String getLocation() {
 		return location;
@@ -170,6 +225,40 @@ public class Task {
 	 */
 	public Boolean getCompleted() {
 		return completed;
+	}
+	
+	/**
+	 * Write a task to the provided PrintWriter object 
+	 * @param writer
+	 */
+	public void write( PrintWriter writer) {
+		// write data separated by tabs
+		StringBuilder s = new StringBuilder();
+		
+		/*
+ 			private short priority;
+			private Date dueDate;
+			private short category;
+			private String description;
+			private String location;
+			private boolean completed;
+		 */
+		
+		s.append(priority);
+		s.append('\t');
+		s.append(dueDate.toString());
+		s.append('\t');
+		s.append(category);
+		s.append('\t');
+		s.append(description);
+		s.append('\t');
+		s.append(location);
+		s.append('\t');
+		s.append(completed);
+		
+		// Ok, write that bad boy
+		writer.println(s);
+		
 	}
 	
 	public Boolean equals(Task task) {
--- TaskList.java
+++ TaskList.java
@@ -1,4 +1,123 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+/**
+ * TaskList contains a list of tasks and operations for that list.
+ * @author Ryan Luchs
+ * 
+ */
 public class TaskList {
+	/*
+	 * Data:
+	 * 
+	 * - list of tasks
+	 */
+	
+	ArrayList<Task> taskList;
+	
+	/*
+	 * Operations
+	 * 
+	 * - add tasks
+	 * - remove tasks
+	 * - search by name
+	 * - get tasks by index
+	 * - search by priority
+	 * - search by due date
+	 * - search by completion
+	 * - search by category
+	 * - sort by priority
+	 * - sort by due date
+	 * - sort by completion
+	 * - sort by category
+	 * - write list to disk
+	 * - read list from disk
+	 */
+	
+	/**
+	 * Create a new TaskList from scratch
+	 */
+	public TaskList(){
+		taskList = new ArrayList<Task>();
+	}
+	
+	/**
+	 * Create a new TaskLst from disk, reading from provided filename.
+	 * @param filename String filename
+	 */
+	public TaskList(String filename) throws FileNotFoundException {
+		readFile(filename);
+	}
+	
+	/**
+	 * Add the provided task.
+	 * @param t
+	 * @return true if add succeeded
+	 */
+	public boolean addTask( Task t) {
+		return taskList.add(t);
+	}
 
+	/**
+	 * Return task at index i. Throws exception if index is illegal.
+	 * @param i index of task
+	 * @return Task object at index.
+	 */
+	public Task getTask(int i) {
+		return taskList.get(i);
+	}
+	
+	/**
+	 * Remove the specified task at index i. Throws exception if index is illegal.
+	 * @param i index of task to remove
+	 */
+	public void removeTask(int i) {
+		taskList.remove(i);
+	}
+	
+	public Task searchByDescription(String d) {
+		Task foo = null;
+		
+		for( int i = 0; i < taskList.size(); i++) {
+			if (taskList.get(i).getDescription().equals(d)) {
+				foo = taskList.get(i);
+				break;
+			}
+		}
+		
+		return foo;
+	}
+	
+	/**
+	 * Read list of tasks from filename provided. Throws exception
+	 * @param filename Filename to read
+	 */
+	private void readFile(String filename) throws FileNotFoundException {
+		//assume that taskList is initialized
+		taskList.clear(); // remove all old tasks
+
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		
+		try {
+			reader.close();
+		} catch(IOException e) {
+			System.out.print("Cannot close file: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * @param filename Filename to write
+	 */
+	public void writeFile(String filename) throws FileNotFoundException{
+		PrintWriter writer = new PrintWriter(filename);
+		for(int i = 0; i < taskList.size(); i++) {
+			taskList.get(i).write(writer);
+		}
+		writer.close();
+	}
 }
